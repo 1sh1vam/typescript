2e12f2f3ae66d9a3e605498144a0792ad242f927
1sh1vam <shivam07.mt@gmail.com>
2022-02-02 19:58:19 +0530

    using typechecking fot function parameter

--- first-app/index.ts
+++ first-app/index.ts
@@ -11,9 +11,14 @@ interface Todo {
 axios.get(url).then((response) => {
     const todo = response.data as Todo;
 
+    const { id, title, completed } = todo;
+    logTodo(id, title, completed)
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
-        The Todo with id: ${todo.id}
-        Has a title of: ${todo.title}
-        Is it completed: ${todo.completed}
+        The Todo with id: ${id}
+        Has a title of: ${title}
+        Is it completed: ${completed}
     `);
-})
+}
