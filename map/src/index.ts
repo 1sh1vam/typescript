36b49d6e155ff2d54345b16ee0267ba30143488b
1sh1vam <shivam07.mt@gmail.com>
2022-02-07 21:13:48 +0530

    hiding all the map methods and only showing few properties and methods using custom map class

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,6 +1,7 @@
 /// <reference types="@types/google.maps" />
 // import { User } from "./User";
 // import { Company } from "./Company";
+import { CustomMap } from './CustomMap';
 
 // const user = new User();
 // const company = new Company();
@@ -8,10 +9,4 @@
 // console.log('user', user);
 // console.log('company', company);
 
-new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+const customMap = new CustomMap();
