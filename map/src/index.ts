db40a2c131fa18b6dcae9a9049b26a99d1457b98
1sh1vam <shivam07.mt@gmail.com>
2022-02-08 20:23:19 +0530

    using refactored addMarker method

--- map/src/index.ts
+++ map/src/index.ts
@@ -10,5 +10,5 @@ const company = new Company();
 // console.log('company', company);
 
 const customMap = new CustomMap('map');
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
